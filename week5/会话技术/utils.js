24be3bb3b557950b1eca4805a530c9cc8afcc854
Fankly <1393452053@qq.com>
2022-09-28 19:31:46 +0800

    test

--- week5/会话技术/utils.js
+++ week5/会话技术/utils.js
@@ -1,10 +1,13 @@
+// 需求：封装getParams方法获取指定get数据格式参数
+
+// https://search.jd.com/Search?keyword=%E8%A2%9C%E5%AD%90&enc=utf-8&pvid=f53def08af8246b3a7102eafda916991
+// decodeURI(地址)
 // https://search.jd.com/Search?keyword=袜子&enc=utf-8&pvid=f53def08af8246b3a7102eafda916991
 
-let params = decodeURI(location.href).split("?").pop
-
-let paramsArr = params.split("&")
-
-
-paramsArr.forEach(item => {
-    
-})
+// 1 按照问号划分成数组然后获取后面的get数据格式参数
+// let params = decodeURI(location.href).split('?')[1]
+let params = decodeURI(location.href).split("?").pop() // 参数名=数据&....&参数名=数据
+// 2 按照【&】划分成数组  ['参数名=数据', ...]
+// 3 遍历 for 遍历  item='参数名=数据'
+// 4 按照【=】划分成数组 temp=[参数名,数据]
+// 5 判断
---
+++ week5/永久登录/axios.js
@@ -0,0 +1,46 @@
+function get(url, params, headers = {}) {
+    const p = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest()
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    let res = JSON.parse(xhr.responseText)
+                    resolve(res)
+                } else {
+                    reject(xhr.status)
+                }
+            }
+        }
+        xhr.open("get", `${url}?${params}`)
+        for (const key in headers) {
+            xhr.setRequestHeader(key, headers[key])
+        }
+        xhr.send()
+    })
+    return p
+}
+
+function post(url, params, headers = {}) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest()
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    let res = JSON.parse(xhr.responseText)
+                    resolve(res)
+                } else {
+                    reject(xhr.status)
+                }
+            }
+        }
+        xhr.open("post", url)
+        xhr.setRequestHeader(
+            "content-type",
+            "application/x-www-form-urlencoded"
+        )
+        for (const key in headers) {
+            xhr.setRequestHeader(key, headers[key])
+        }
+        xhr.send(params)
+    })
+}
